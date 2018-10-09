203cea9b2ed5d5886705442a072bde1fc39aa8a3
matt <matt@matt-Surface-Pro-3-Ubuntu>
2018-10-09 12:14:54 -0400

    Working out bugs. See Dispatcher::doTick()

--- Proj2_410_queues_SKELETON/dispatcher/dispatcher.cpp
+++ Proj2_410_queues_SKELETON/dispatcher/dispatcher.cpp
@@ -21,6 +21,11 @@ void reinitializePCB() {
 	runningPCB.start_time = UNINITIALIZED;
 }
 
+void markPCBInvalid()
+{
+	runningPCB.cpu_time = UNIMPLEMENTED;
+}
+
 void dispatcher::init() {
 	clear(ready_Q);
 	clear(blocked_Q);
@@ -102,6 +107,11 @@ int dispatcher::doTick() {
 		if (runningPCB.cpu_time > 0) {
 			runningPCB.cpu_time -= 1;
 			//if it is now still above zero, return
+
+			/**
+			 * This block also isn't the cause of my bug below, Debugged and it still happens.
+			 * Happens at line 134
+			 */
 			if (runningPCB.cpu_time > 0) {
 				return PCB_CPUTIME_DECREMENTED;
 			}
@@ -109,6 +119,13 @@ int dispatcher::doTick() {
 		}
 		//otherwise, if it is 0 to start with, proceed
 		if (runningPCB.cpu_time == 0) {
+
+			/**
+			 * So weird bug I need to work. If we subtract 1 and then cpu_time becomes 0,
+			 * eg we are now here, we eventually reinitialize the PCB. But then the tester
+			 * goes to grab the runningPCB to log it, but that PCB is uninitialized
+			 */
+
 			//process is done, move on
 			int rtn;
 
@@ -118,12 +135,28 @@ int dispatcher::doTick() {
 				runningPCB.io_time = 0;
 				blocked_Q.push(runningPCB);
 				rtn = PCB_ADDED_TO_BLOCKED_QUEUE;
-				reinitializePCB();
+				markPCBInvalid();
 			} else {
-				reinitializePCB();
+				/**
+				 * This block is what is causing my bug above. Following the flowchart
+				 * means I need to mark the PCB as invalid, as such with my reinitialize method
+				 * but then it returns PCB finished, which in turns forces the tester to grab the process_number
+				 * for logging, but that number is now -5 because I'm supposed to reinitialize
+				 */
+				markPCBInvalid();
 				rtn = PCB_FINISHED;
 			}
 			return rtn;
+		}
+		/**
+		 * I actually really hate this fix lol. So when we finish the doTick and return "something"
+		 * instead of uninitializing the whole PCB we mark cpu_time as UNIMPLEMENTED (-6 or something) and check for that inside
+		 * of doTick. NO_JOBS may not be the best return for right now, but I'll do more testing soon.
+		 */
+		else if(runningPCB.cpu_time == UNIMPLEMENTED)
+		{
+			reinitializePCB();
+			return NO_JOBS;
 		}
 	}
 }
